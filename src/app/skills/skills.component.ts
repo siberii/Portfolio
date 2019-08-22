1237c63d2dbef6cc4cdb4bc0bb0db0af9ac0dd9f
Simon Berhanu <simon1515@hotmail.com>
2019-08-22 17:24:21 -0400

    Add skills page

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  isBasic = false;
+  isIntermediate = true;
 
   constructor() { }
 
+  toggleLevel(event): void {
+    if (event.target.innerText === 'Basic') {
+      this.isBasic = true;
+      this.isIntermediate = false;
+    } else {
+      this.isBasic = false;
+      this.isIntermediate = true;
+    }
+
+  }
   ngOnInit() {
   }
 
