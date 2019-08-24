c68189f3eb5d37bfd197b6b2662f663378071082
Simon Berhanu <simon1515@hotmail.com>
2019-08-23 23:30:55 -0400

    Add breadcrums to desktop version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,28 @@ import { Component, ViewChild, AfterViewInit } from '@angular/core';
 export class AppComponent implements AfterViewInit {
   @ViewChild('hamburgerRef', { static: true }) hamburgerElemRef;
   @ViewChild('homeRef', { static: true }) homeElemRef;
+  @ViewChild('projectsRef', { static: true }) projectsElemRef;
 
   isScrolled: boolean;
+  isScrolledCrums: boolean;
+
+  isFirstCrum = true;
+  isSecondCrum: boolean;
+  isThirdCrum: boolean;
+
   hamburgerElement: Element;
   homeElement: Element;
+  crumsElement: Element;
+  projectsElement: Element;
 
   ngAfterViewInit(): void {
     this.hamburgerElement = this.hamburgerElemRef.hamburgerElemRef.nativeElement;
     this.homeElement = this.homeElemRef.heroElemRef.nativeElement;
+    this.crumsElement = this.hamburgerElemRef.crumsElemRef.nativeElement;
+    this.projectsElement = this.projectsElemRef.heroElemRef.nativeElement;
     this.invertHamburger();
+    this.invertCrums();
+    this.invertThirdCrum();
   }
 
   invertHamburger() {
@@ -41,4 +54,52 @@ export class AppComponent implements AfterViewInit {
     homeObserver.observe(this.homeElement);
   }
 
+  invertCrums() {
+    const homeOption = {
+      rootMargin: '-450px 0px 0px 0px'
+    };
+
+    const homeObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (!entry.isIntersecting) {
+            this.isScrolledCrums = true;
+            this.isSecondCrum = true;
+            this.isFirstCrum = false;
+            this.isThirdCrum = false;
+          } else {
+            this.isScrolledCrums = false;
+            this.isSecondCrum = false;
+          }
+        });
+      },
+      homeOption
+    );
+
+    homeObserver.observe(this.homeElement);
+  }
+
+  invertThirdCrum() {
+    const homeOption = {
+      rootMargin: '0px 0px 0px 0px'
+    };
+
+    const homeObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (!entry.isIntersecting) {
+            this.isThirdCrum = false;
+            this.isSecondCrum = true;
+          } else {
+            this.isThirdCrum = true;
+            this.isFirstCrum = false;
+            this.isSecondCrum = false;
+          }
+        });
+      },
+      homeOption
+    );
+
+    homeObserver.observe(this.projectsElement);
+  }
 }
--- src/app/hamburger/hamburger.component.ts
+++ src/app/hamburger/hamburger.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hamburger',
@@ -9,36 +9,51 @@ export class HamburgerComponent implements OnInit {
   constructor() {
   }
   isChecked = false;
+  atHomeSection: boolean;
+  atAboutSection: boolean;
+  atProjectsSection: boolean;
 
   @ViewChild('hamburgerRef', { static: true }) hamburgerElemRef: ElementRef;
+  @ViewChild('headerRef', { static: true }) headerElemRef: ElementRef;
+  @ViewChild('crumsRef', { static: true }) crumsElemRef: ElementRef;
   @Input() isScrolled: boolean;
+  @Input() isScrolledCrums: boolean;
+
+  @Input() isFirstCrum: boolean;
+  @Input() isSecondCrum: boolean;
+  @Input() isThirdCrum: boolean;
   @Input() home: any;
   @Input() about: any;
   @Input() projects: any;
   @Input() contact: any;
 
   onChange(e) {
-    console.log(e.target.checked);
     this.isChecked = e.target.checked;
   }
 
-  onHomeClick() {
+  onHomeClick(isHeader: HTMLElement) {
     this.home.heroElemRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
-    this.isChecked = !this.isChecked;
+    if (!isHeader) {
+      this.isChecked = !this.isChecked;
+    }
   }
-  onAboutClick() {
+  onAboutClick(isHeader: HTMLElement) {
     this.about.heroElemRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
-    this.isChecked = !this.isChecked;
+    if (!isHeader) {
+      this.isChecked = !this.isChecked;
+    }
   }
-  onProjectsClick() {
-    console.log(this.projects);
-
+  onProjectsClick(isHeader: HTMLElement) {
     this.projects.heroElemRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
-    this.isChecked = !this.isChecked;
+    if (!isHeader) {
+      this.isChecked = !this.isChecked;
+    }
   }
-  onContactClick() {
+  onContactClick(isHeader: HTMLElement) {
     this.contact.heroElemRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
-    this.isChecked = !this.isChecked;
+    if (!isHeader) {
+      this.isChecked = !this.isChecked;
+    }
   }
 
   ngOnInit() {
