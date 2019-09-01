110204bfc2b94c80e642eb87751c692b66c15bbc
Simon Berhanu <simon1515@hotmail.com>
2019-09-01 01:53:32 -0400

    Add particles background

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+// import { particlesJS } from './../../main';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
   @ViewChild('heroRef', { static: true }) heroElemRef: ElementRef;
   @Input() target: any;
-
-
+  particlesJS: any;
   isActive = false;
+
+  loadParticles() {
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    // @ts-ignore
+    particlesJS.load('particles-js', './assets/nasa.json', () => {
+      console.log('callback - particles-js config loaded');
+    });
+  }
   animateOut() {
     this.isActive = true;
   }
@@ -19,5 +27,6 @@ export class HomeComponent implements OnInit {
     this.target.heroElemRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
   }
   ngOnInit() {
+    this.loadParticles();
   }
 }
